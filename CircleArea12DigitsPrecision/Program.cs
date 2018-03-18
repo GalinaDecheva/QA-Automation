20516e45e090ff96e9bd285685d7763fd5cae257
Galina Decheva <de_galina@abv.bg>
2018-03-18 15:36:21 +0200

    First commit

---
+++ CircleArea12DigitsPrecision/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea12DigitsPrecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double result = Math.PI * radius * radius;
+            //Console.WriteLine("{0} is area on circle with radius {1}",result,radius);
+            Console.WriteLine("{0:f12}", result);
+        }
+    }
+}

1c7865e33a093de3b641108bf15a752b7d2a0f94
Galina Decheva <de_galina@abv.bg>
2018-03-18 20:22:11 +0200

    Adding RemoveNegativesAndReverse

---
+++ RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.Reverse();
+            numbers.RemoveAll(x => x < 0);
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", numbers));
+            }
+            
+        }
+    }
+}
